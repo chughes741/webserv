123ddd805e1796349fd2fd85045b8d485080fcdd
Francis Lahoud <francislah@hotmail.com>
2023-04-13 10:54:58 -0400

    setup configBlock class

---
+++ include/configBlock.hpp
@@ -0,0 +1,37 @@
+#ifndef CONFIGBLOCK_HPP
+ #define CONFIGBLOCK_HPP
+
+# include <iostream>
+# include <vector>
+
+//Template 
+class ConfigBlock {
+public:
+//Default constructor (Required)
+	ConfigBlock();
+//Copy constructor (Required)
+	ConfigBlock (const ConfigBlock &copy);
+//Constructors
+//Default Destructor (Required)
+	~ConfigBlock();
+//Destructors
+//Copy assignment operator (Required)
+	ConfigBlock & operator = (const ConfigBlock &copy);
+//Operators
+//Swap function
+//Setters & Getters
+//Other functions
+	bool validateType(); //from enum launch pointer function to validate type
+protected:
+
+private:
+	//Enum blockType? Events;http;server;location
+	std::string blockType;
+	std::vector<ConfigBlock>
+
+	//All configurations available
+	//server_name ...;
+
+};
+
+#endif
