93f63d077d3f395cafb5287187645b7d67ea55c6
Cole Hughes <coltenhughes@gmail.com>
2023-04-20 13:09:48 -0400

    Updated docs

--- docs/classServer.js
+++ docs/classServer.js
@@ -3,7 +3,7 @@ var classServer =
     [ "~Server", "classServer.html#a9fd0bddb553794aa53950f97b46954bc", null ],
     [ "createSocket", "classServer.html#a0a146161f39d70e0ad886ba1c0f7d70c", null ],
     [ "handleRequest", "classServer.html#a18693651f290ef4dfb9077b592349409", null ],
-    [ "readRequest", "classServer.html#ac8767c8f6f17941588a3ba1a4989297e", null ],
+    [ "receiveRequest", "classServer.html#a1d3fd3a753bd3a1599c28b8413635658", null ],
     [ "sendResponse", "classServer.html#ab7c284f187e9a2b53bc56ab1ee55fe66", null ],
     [ "start", "classServer.html#aa49a1de2493972fef47c9fbfd04a3b97", null ],
     [ "stop", "classServer.html#a4d30c699c5d1f119cbdd5b7e3bf624e6", null ],
--- docs/search/variables_c.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['uri_125',['uri',['../structRequest.html#a1c2e4f1e54a9ea1ea455d513a18c5fd1',1,'Request']]],
-  ['use_126',['use',['../structEventsConfig.html#ab27090310e6c22a9ece3b4943db5b2f1',1,'EventsConfig']]],
-  ['use_5fpoll_127',['use_poll',['../structEventsConfig.html#a82e45599525b8ee75a1bb2d79208fa9b',1,'EventsConfig']]]
-];
