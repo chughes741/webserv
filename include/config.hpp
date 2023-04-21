889ebe94df7985655ace30233618d67034133ebe
Cole <95713039+chughes741@users.noreply.github.com>
2023-04-21 14:10:01 -0400

    Moved macros in events to config

--- include/config.hpp
+++ include/config.hpp
@@ -68,10 +68,55 @@
 #include "events.hpp"
 #include "webserv.hpp"
 
+/** HTTP methods */
+#define POST 0x01
+#define GET 0x02
+#define DELETE 0x04
+
+/** HTTP Status codes */
+#define OK "200 OK"
+#define CREATED "201 Created"
+#define ACCEPTED "202 Accepted"
+#define NO_CONTENT "204 No Content"
+#define MOVED_PERMANENTLY "301 Moved Permanently"
+#define FOUND "302 Found"
+#define NOT_MODIFIED "304 Not Modified"
+#define BAD_REQUEST "400 Bad Request"
+#define NOT_FOUND "404 Not Found"
+#define METHOD_NOT_ALLOWED "405 Method Not Allowed"
+#define IM_A_TEAPOT "418 I'm a teapot"
+#define INTERNAL_SERVER_ERROR "500 Internal Server Error"
+#define BAD_GATEWAY "502 Bad Gateway"
+
+/** HTTP headers */
+#define HTTP_VERSION "HTTP/1.1"
+
 using std::map;
 using std::pair;
 using std::string;
 using std::vector;
+
+/**
+ * @brief Represents an HTTP request
+ */
+struct Request {
+    string              method;  /**< HTTP method (GET, POST, etc.) */
+    string              uri;     /**< Request URI */
+    string              version; /**< HTTP version */
+    map<string, string> headers; /**< Other headers */
+    string              body;    /**< Request body (if any) */
+};
+
+/**
+ * @brief Represents an HTTP response
+ */
+struct Response {
+    string              version; /**< HTTP version */
+    string              status;  /**< HTTP status code and message */
+    string              server;  /**< Value of the Server header */
+    map<string, string> headers; /**< Other headers */
+    string              body;    /**< Response body (if any) */
+};
 
 /**
  * @brief Configuration options for a single location block
