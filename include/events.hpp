20e1dbc3210a94da4a1ab4044e051d26b304b29e
Cole Hughes <coltenhughes@gmail.com>
2023-04-24 11:31:05 -0400

    Added EventLoop for Linux/MacOS

--- include/events.hpp
+++ include/events.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#ifdef __APPLE__
 #include <sys/event.h>
+#elif __linux__
+#include <sys/epoll.h>
+#endif  // __APPLE__
 #include <sys/time.h>
 #include <sys/types.h>
 
@@ -10,39 +14,62 @@ using std::map;
 using std::runtime_error;
 
 /**
- * @brief KQueue class for handling events
+ * @brief Abstract class for handling event
+ */
+class EventLoop {
+   public:
+    virtual ~EventLoop();
+
+    virtual void start()                            = 0;
+    virtual void stop()                             = 0;
+    virtual void registerSocket(int fd, int events) = 0;
+    virtual void unregisterSocket(int fd)           = 0;
+
+   protected:
+};
+
+#ifdef __APPLE__
+/**
+ * @brief KqueueEventLoop class for handling events, used on MacOS
  *
  * @details This class wraps around the kqueue system call and provides
  * a simple interface for adding and removing sockets from the kqueue.
  */
-class KQueue {
+class KqueueEventLoop : public EventLoop {
    public:
-    /**
-     * @brief Creates kqueue. Does not run if one already exists
-     */
-    static void createQueue();
+    KqueueEventLoop();
 
-    /**
-     * @brief Adds a socket to the kqueue
-     *
-     * @param fd file descriptor of the socket
-     */
-    static void addSocket(int fd);
-
-    /**
-     * @brief Removes a socket from the kqueue
-     *
-     * @param fd file descriptor of the socket
-     */
-    static void removeSocket(int fd);
-
-    /**
-     * @brief Waits for events on the kqueue
-     */
-    static struct kevent eventListen();
+    void start();
+    void stop();
+    void registerSocket(int fd, int events);
+    void unregisterSocket(int fd);
 
    private:
-    static int                     queue_fd_; /**< kqueue file descriptor */
-    static struct timespec         timeout_;  /**< timeout for kevent */
-    static map<int, struct kevent> events_;   /**< ident, event parameters */
+    struct kevent eventListen();
+
+   private:
+    int                     queue_fd_; /**< kqueue file descriptor */
+    struct timespec         timeout_;  /**< timeout for kevent */
+    map<int, struct kevent> events_;   /**< ident, event parameters */
 };
+#elif __linux__
+/**
+ * @brief Epoll class for handling events, used on Linux
+ *
+ * @details This class wraps around the epoll system call and provides
+ * a simple interface for adding and removing sockets from the epoll.
+ */
+class EpollEventLoop : public EventLoop {
+   public:
+    EpollEventLoop();
+
+    void start();
+    void stop();
+    void registerSocket(int fd, int events);
+    void unregisterSocket(int fd);
+
+   private:
+    int                          epoll_fd_; /**< epoll file descriptor */
+    map<int, struct epoll_event> events_;   /**< ident, event parameters */
+};
+#endif  // __APPLE__
