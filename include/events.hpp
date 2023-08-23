d355aed8aa8c96bcc875c74fbd19230aed81abbe
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2023-08-23 12:53:05 -0400

    Removed comment from events.hpp

--- include/events.hpp
+++ include/events.hpp
@@ -1,37 +1,3 @@
-/**
- * @file events.hpp
- * @brief Declares classes for handling events on sockets using the kqueue or
- * epoll system calls.
- *
- * This file contains the declaration of the abstract EventListener class and
- * its two derived classes, KqueueEventListener and EpollEventListener. The
- * EventListener class provides an interface for handling events on sockets,
- * such as registerEventing and unregisterEventing sockets for read and write
- * events. The KqueueEventListener and EpollEventListener classes inherit from
- * the EventListener class and implement the event handling functionality using
- * the kqueue and epoll system calls, respectively.
- *
- * To use the EventListener classes, you must create an instance of one of the
- * derived classes, registerEvent the sockets you want to monitor using the
- * registerEventSocket() method, and then call the start() method to begin
- * monitoring events on the registerEvented sockets. Once you have received an
- * event, you can use the handleEvent() method to process the event and take
- * appropriate action, such as reading or writing data to the socket.
- *
- * @note The KqueueEventListener class is used on MacOS, and the
- * EpollEventListener class is used on Linux.
- *
- * @note This code is for educational purposes only and should not be used in
- * production environments without extensive testing and modification.
- *
- * @version 0.1
- * @date 2023-04-24
- * @authors
- *   - Francis L.
- *   - Marc-André L.
- *   - Cole H.
- */
-
 #pragma once
 
 #include <fcntl.h>
