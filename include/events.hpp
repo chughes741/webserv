b72e85b7ca82fc8cc4601e32b76d28ef3868a5f7
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2023-08-23 12:54:10 -0400

    Created an InternalEvent enum

--- include/events.hpp
+++ include/events.hpp
@@ -12,47 +12,41 @@
 
 #include "logging.hpp"
 
-#define READABLE         1
-#define WRITABLE         2
-#define ERROR_EVENT      4
-#define CONNECT_EVENT    8
-#define DISCONNECT_EVENT 16
-
 typedef uint32_t KqueueEvent;
 typedef uint32_t InternalEvent;
 
-/**
- * @brief Abstract class for handling event
- */
+enum EventType : InternalEvent {
+    NONE             = 0,
+    READABLE         = 1,
+    WRITABLE         = 2,
+    ERROR_EVENT      = 4,
+    SIGNAL_EVENT     = 8,
+    CONNECT_EVENT    = 16,
+    DISCONNECT_EVENT = 32
+};
+
+// Abstract base class for event handlers
 class EventListener {
    public:
     virtual ~EventListener();
 
-    virtual std::pair<int, InternalEvent> listen()                          = 0;
-    virtual bool                          registerEvent(int fd, int events) = 0;
-    virtual void                          unregisterEvent(int fd)           = 0;
+    virtual std::pair<int, InternalEvent> listen()                                    = 0;
+    virtual bool                          registerEvent(int fd, InternalEvent events) = 0;
+    virtual void                          unregisterEvent(int fd)                     = 0;
 };
 
-/** Event map to convert KqueueEvents to internal events */
-
-/**
- * @brief KqueueEventListener class for handling events, used on MacOS
- *
- * @details This class wraps around the kqueue system call and provides
- * a simple interface for adding and removing sockets from the kqueue.
- */
+// Kqueue event handler
 class KqueueEventListener : public EventListener {
    public:
     KqueueEventListener();
 
     std::pair<int, InternalEvent> listen();
-    bool                          registerEvent(int fd, int events);
+    bool                          registerEvent(int fd, InternalEvent events);
     void                          unregisterEvent(int fd);
 
    private:
-    int                                  queue_fd_; /**< kqueue file descriptor */
-    struct timespec                      timeout_;  /**< timeout for kevent */
-    std::map<int, struct kevent>         events_;   /**< ident, event parameters */
-    std::map<KqueueEvent, InternalEvent> KqueueEventMap;
+    int                                  queue_fd_;       // kqueue file descriptor
+    struct timespec                      timeout_;        // timeout for kevent
+    std::map<int, struct kevent>         events_;         // ident, event parameters
+    std::map<KqueueEvent, InternalEvent> KqueueEventMap;  // map for converting events
 };
-
