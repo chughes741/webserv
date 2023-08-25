497ed66eb52d88d5531e618ee8e6710534bc4c79
Francis Lahoud <flahoud@student.42quebec.com>
2023-08-24 23:48:28 -0400

    Added template animated 404 page
    credits: https://github.com/tsparticles/404-templates

---
+++ html/Error/js/404.js
@@ -0,0 +1,53 @@
+var particles = {
+    background: {
+        color: {
+            value: "#0d47a1"
+        }
+    },
+    fpsLimit: 60,
+    particles: {
+        color: {
+            value: "#ffffff"
+        },
+        links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1
+        },
+        collisions: {
+            enable: true
+        },
+        move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 3,
+            straight: false
+        },
+        number: {
+            density: {
+                enable: true,
+                area: 800
+            },
+            value: 80
+        },
+        opacity: {
+            value: 0.5
+        },
+        shape: {
+            type: "circle"
+        },
+        size: {
+            random: true,
+            value: 5
+        }
+    },
+    detectRetina: true
+};
+
+
+tsParticles.load("tsparticles", particles);
+// All credits to https://github.com/tsparticles/404-templates
