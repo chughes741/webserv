3356287e9d07820b1a6f21eac171ed3244894153
Francis Lahoud <flahoud@student.42quebec.com>
2023-08-31 23:41:02 -0400

    Implement some test for default nginx.conf

--- test/parsing_test.cpp
+++ test/parsing_test.cpp
@@ -4,16 +4,60 @@
 #include <gtest/gtest.h>
 
 #include "config.hpp"
-#include "webserv.hpp"
+#include "parsing.hpp"
+
+#define TEST_CONFIG "../test/parsing_test.conf"
+
+TEST(parsingTest, GlobalUser) {
+    HttpConfig httpConfig;
+    parseConfig(TEST_CONFIG, httpConfig);
+    EXPECT_EQ(httpConfig.user, "nobody");
+}
+
+TEST(parsingTest, GlobalWorkerProcesses) {
+    HttpConfig httpConfig;
+    parseConfig(TEST_CONFIG, httpConfig);
+    EXPECT_EQ(httpConfig.worker_processes, 1);
+}
 
 TEST(parsingTest, GlobalErrorLog) {
     HttpConfig httpConfig;
-    parseConfig("../test/parsing_test.conf", httpConfig);
+    parseConfig(TEST_CONFIG, httpConfig);
     EXPECT_EQ(httpConfig.error_log, "logs/error.log");
 }
 
-TEST(parsingTest, GlobalWorkerConnections) {
+TEST(parsingTest, EventsWorkerConnections) {
     HttpConfig httpConfig;
-    parseConfig("../test/parsing_test.conf", httpConfig);
-    EXPECT_EQ(httpConfig.error_log, "logs/error.log");
+    parseConfig(TEST_CONFIG, httpConfig);
+    EXPECT_EQ(httpConfig.worker_connections, 2048);
+}
+
+TEST(parsingTest, LocationRoot) {
+    HttpConfig httpConfig;
+    parseConfig(TEST_CONFIG, httpConfig);
+    EXPECT_EQ(httpConfig.servers[0].locations["/"].root, "html");
+}
+
+TEST(parsingTest, LocationDiffRoot) {
+    HttpConfig httpConfig;
+    parseConfig(TEST_CONFIG, httpConfig);
+    EXPECT_EQ(httpConfig.servers[1].locations["/"].root, "pas-html");
+}
+
+TEST(parsingTest, ServerName) {
+    HttpConfig httpConfig;
+    parseConfig(TEST_CONFIG, httpConfig);
+    EXPECT_EQ(httpConfig.servers[2].server_names[0], "localhost");
+}
+
+TEST(parsingTest, ServerErrorPage) {
+    HttpConfig httpConfig;
+    parseConfig(TEST_CONFIG, httpConfig);
+    EXPECT_EQ(httpConfig.servers[0].error_page[500], "/500.html");
+}
+
+TEST(parsingTest, HttpErrorPage) {
+    HttpConfig httpConfig;
+    parseConfig(TEST_CONFIG, httpConfig);
+    EXPECT_EQ(httpConfig.error_page[500], "/500.html");
 }
