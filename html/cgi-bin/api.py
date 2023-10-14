76e3dfd0946a24fbacdbd64d1dedcdc9b6afecaf
fenix144 <yan_1731@hotmail.com>
2023-10-14 19:37:06 -0400

    Add basic script

---
+++ html/cgi-bin/api.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import cgi
+
+print("Content-type: text/html")
+print()
+
+print("<html><body>")
+print("<h1>Hello from Python CGI!</h1>")
+form = cgi.FieldStorage()
+if "name" in form:
+    print("<p>Hello, " + form["name"].value + "!</p>")
+else:
+    print("<p>Hello, anonymous!</p>")
+print("</body></html>")
