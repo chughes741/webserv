878e97823ad24f911957be85fb478a0628d1b5f7
Francis Lahoud <flahoud@student.42quebec.com>
2023-10-22 22:43:59 -0400

    add client_max_body_size error status

--- include/http.hpp
+++ include/http.hpp
@@ -30,6 +30,7 @@ enum HttpStatus {
     BAD_REQUEST           = 400,
     NOT_FOUND             = 404,
     METHOD_NOT_ALLOWED    = 405,
+    CONTENT_TOO_LARGE     = 413,
     IM_A_TEAPOT           = 418,
     INTERNAL_SERVER_ERROR = 500,
     BAD_GATEWAY           = 502
